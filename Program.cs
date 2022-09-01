2bfdf89afef10d96fc4ccc46e6c58d1b0bf17b3c
Anastasia <nastyapanda1107@gmail.com>
2022-09-02 00:26:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введи 2 числа");
+ int a, b;
+ Console.WriteLine("Введи число ");
+ a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введи число ");
+ b = Convert.ToInt32(Console.ReadLine());
+ bool c = a > b;
+ if (c)
+ { Console.WriteLine($"Число {a} больше чем {b}");}
+
+ else
+{ Console.WriteLine($"Число {b} больше чем {a}");}
+
+
+
+int a;                             
+Console.Write("введите число: ");
+a= Console.Read();
+ if (a % 2==1)
+{
+Console.WriteLine("число нечетное");}
+else
+{
+Console.WriteLine("Число четное");}
